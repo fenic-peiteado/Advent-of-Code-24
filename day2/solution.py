fcd7c21287ec2a9bc3546e51f801a48fcc032881
Jose Fenic Peiteado Padilla <fenic43ultimate@gmail.com>
2024-12-03 00:22:37 +0100

    Practicando python con otras soluciones

--- day2/solution.py
+++ day2/solution.py
@@ -2,27 +2,25 @@ import sys
 
 
 datos = sys.stdin.read().strip()
-
 lines = datos.split('\n')
-left = []
-right = []
-map_right = {}
+counts = 0
+counts2 = 0
+# Solucion alternativa para comprobar metodos de python
+# Slice
+# Sorted
 for i in lines:
-  element = i.split()
-  print(element)
-  left.append(int(element[0]))
-  right.append(int(element[1]))
-  map_right[int(element[1])] = map_right.get(int(element[1]), 0) + 1
-
-left = sorted(left)
-right = sorted(right)
-print(left)
-print(right)
-result = 0
-result2 = 0
-for i in range(0, len(left)):
-  result +=  abs(left[i] - right[i])
-for i in range(0, len(left)):
-  result2 += int(left[i]) * map_right.get(left[i], 0)
-print(result)
-print(result2)
+  le = list(map(int,i.split()))
+  exist_solution = False
+  for i in range(len(le)):
+    le_short = le[:i] + le[i+1:]
+    type_or = (le_short == sorted(le_short) or le_short == sorted(le_short, reverse=True))
+    check = True
+    for i in range(len(le_short)-1):
+      dif = abs(le_short[i] - le_short[i+1])
+      if (not( 1 <= dif <= 3 )):
+        check = False
+    if (check and type_or):
+      exist_solution = True
+  if(exist_solution):
+   counts2 += 1   
+print(counts2)
